430198e1a9f18392e235d1348568a96bbaf6e96f
Kunhalmi Attila <79263624+anthonyboss97@users.noreply.github.com>
2025-05-12 15:26:08 +0200

    Add basic CodeGeneration Test project

---
+++ ProtonComplier/test/CodeGeneration.Tests/CodeGenTests.cs
@@ -0,0 +1,19 @@
+global using NUnit.Framework;
+using Moq;
+using Proton.ErrorHandler;
+using Proton.Lexer;
+using Proton.Lexer.Enums;
+using Proton.Lexer.Interfaces;
+using Proton.Lexer.Services;
+
+namespace CodeGeneration.Tests
+{
+    [TestFixture]
+    public class CodeGenTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
